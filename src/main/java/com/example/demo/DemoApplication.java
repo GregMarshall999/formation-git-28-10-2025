1aea04cd5267e90fdd264c2bab297ee719782bc4
Gregory <gregory.marshall999@gmail.com>
2025-10-27 14:54:59 +0100

    if verifie chiffre est entier

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,11 +14,11 @@ public class DemoApplication {
 		System.out.println("Commit avant checkout vers dev, puis Un commit depuis dev");
 
 		int chiffre = 1;
-		if(chiffre % 2 == 0) {
+		if(Integer.parseInt(String.valueOf(chiffre)) == (int)chiffre && chiffre % 2 == 0) {
 			System.out.println("Pair");
 		}
 		else {
-			System.out.println("Impair");
+			System.out.println("Impair ou non chiffre");
 		}
 
 		//SpringApplication.run(DemoApplication.class, args);
